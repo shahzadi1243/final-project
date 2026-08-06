60ab774d7dee616c2f6063194304aea76a744d96
shahzadirai <shahzadirai55@gmail.com>
2026-08-06 13:33:03 +0500

    intail commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HouseSlider from "./modules/House Designs/hero";
 import HouseFilterSection from "./modules/House Designs/heer";
 import HousePlansPortal from "./modules/House Designs/heer2";
+import HousePlansPortal from "./modules/house Designs/heer3";
 export default function HouseDesignsPage() {
   return (
     <>
       <HouseSlider />
       <HouseFilterSection/>
+      <HousePlansPortal/>
       <HousePlansPortal/>
       </>
     </>
